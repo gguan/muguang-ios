27384bb2c67e466b0adfa8552d9123d005e5be26
Hao Zhang <zhanghao.dev@gmail.com>
2015-04-15 14:49:54 +0800

    Revert 8c017c3..a8d8eb7
    This rolls back to commit 8c017c35e8e1a0d4ac10cf7a220281821803121b.

---
+++ MuGuangIOS/MGMainViewController.swift
@@ -0,0 +1,13 @@
+//
+//  MGMainViewController.swift
+//  MuGuangIOS
+//
+//  Created by ZH on 15/4/15.
+//  Copyright (c) 2015年 北京目光璀璨科技有限公司. All rights reserved.
+//
+
+import UIKit
+
+class MGMainViewController: MGBaseViewController {
+   
+}
