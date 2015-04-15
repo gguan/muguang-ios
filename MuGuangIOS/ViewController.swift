38488b314fbbbfbf20bdcd47cf1a933bee676ebb
William Hu <william.hqs@gmail.com>
2015-04-15 10:58:00 +0800

    init code

---
+++ MuGuangIOS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MuGuangIOS
+//
+//  Created by William Hu on 15/4/15.
+//  Copyright (c) 2015 William Hu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
