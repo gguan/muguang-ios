d3981b4e95b5ad338a3279257232071d40ee8ca6
Hao Zhang <zhanghao.dev@gmail.com>
2015-04-15 16:01:22 +0800

    删除Bridging文件冲突

--- MuGuangIOS/MuGuangIOS-Bridging-Header.h
+++ MuGuangIOS/MuGuangIOS-Bridging-Header.h
@@ -2,9 +2,6 @@
 //  Use this file to import your target's public headers that you would like to expose to Swift.
 //
 
-<<<<<<< HEAD
 #import <Masonry.h>
-=======
 #import <ShareSDK/ShareSDK.h>
 #import "WeiboSDK.h"
->>>>>>> origin/master
