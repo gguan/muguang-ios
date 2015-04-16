4dd6e86181190794e46fd2a641ad9e6cc2b9b191
Hao Zhang <zhanghao.dev@gmail.com>
2015-04-16 12:18:28 +0800

    首页增加相机

--- MuGuangIOS/MGCameraView.swift
+++ MuGuangIOS/MGCameraView.swift
@@ -9,15 +9,21 @@
 import UIKit
 import AVFoundation
 
+/**
+ *  主页面的相机类
+ */
 class MGCameraView: UIView {
     let captureSession: AVCaptureSession = AVCaptureSession()
     let stillImageOutput: AVCaptureStillImageOutput = AVCaptureStillImageOutput()
     let previewLayer: AVCaptureVideoPreviewLayer = AVCaptureVideoPreviewLayer()
-
+    // 闭包回调
+    var tapAction: ((isRunning: Bool) -> Void)? = nil
     override init(frame: CGRect) {
-        captureSession.sessionPreset = AVCaptureSessionPresetPhoto
         
+        // 配置相机
+        captureSession.sessionPreset = AVCaptureSessionPresetPhoto
         var device: AVCaptureDevice?
+        // 加判断防止模拟器crash
         #if arch(i386) || arch(x86_64)
             //simulator
             #else
@@ -26,9 +32,6 @@ class MGCameraView: UIView {
         #endif
         if let captureDevice = device {
             if captureDevice.lockForConfiguration(nil) {
-                if captureDevice.smoothAutoFocusSupported {
-                    captureDevice.smoothAutoFocusEnabled = true
-                }
                 if captureDevice.isFocusModeSupported(.AutoFocus) {
                     captureDevice.focusMode = .AutoFocus;
                 }
@@ -50,19 +53,55 @@ class MGCameraView: UIView {
         stillImageOutput.outputSettings = outputSettings as [NSObject : AnyObject]
         
         captureSession.addOutput(stillImageOutput)
+        previewLayer.session = captureSession
         previewLayer.videoGravity = AVLayerVideoGravityResizeAspectFill
-        
         super.init(frame: frame)
+        self.layer.addSublayer(previewLayer)
+        
+        // 添加点击手势
+        var tapGR: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: Selector("methodForTapGestureRecognizer:"))
+        self.addGestureRecognizer(tapGR)
     }
     
     required init(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
     }
     
+    // 添加点击后执行的方法
+    func addTapAction(aClosure: (isRunning: Bool) -> Void) -> Void {
+        self.tapAction = aClosure
+    }
+    
+    /**
+     *点击手势
+     */
+    func methodForTapGestureRecognizer(tap: UITapGestureRecognizer!) {
+        if let action = self.tapAction {
+            action(isRunning: self.captureSession.running)
+        }
+    }
+    
+    /**
+     *开始运行
+     */
+    func startRunning() {
+        var fadeAnimation = CATransition()
+        fadeAnimation.type = kCATransitionFade
+        fadeAnimation.duration = 0.25
+        self.previewLayer.addAnimation(fadeAnimation, forKey: "Fade")
+        self.captureSession.startRunning()
+    }
+    
+    /**
+     *停止运行
+     */
+    func stopRunning() {
+        self.captureSession.stopRunning()
+    }
+    
     override func layoutSubviews() {
         super.layoutSubviews()
         self.previewLayer.frame = self.frame
-        self.layer.addSublayer(previewLayer)
     }
     
     /*
