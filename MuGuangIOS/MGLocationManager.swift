f9f06f0637543abc5555fc7b9d04b24b2ed57ac6
ZhangHao <ZH@ZhangHaodeiMac.local>
2015-04-20 14:07:27 +0800

    添加位置管理器以及位置数据的View

---
+++ MuGuangIOS/MGLocationManager.swift
@@ -0,0 +1,13 @@
+//
+//  MGLocationManager.swift
+//  MuGuangIOS
+//
+//  Created by ZhangHao on 15/4/20.
+//  Copyright (c) 2015年 北京目光璀璨科技有限公司. All rights reserved.
+//
+
+import UIKit
+
+class MGLocationManager: NSObject {
+   
+}
---
+++ MuGuangIOS/MGMonitorView.swift
@@ -0,0 +1,21 @@
+//
+//  MGMonitorView.swift
+//  MuGuangIOS
+//
+//  Created by ZhangHao on 15/4/20.
+//  Copyright (c) 2015年 北京目光璀璨科技有限公司. All rights reserved.
+//
+
+import UIKit
+
+class MGMonitorView: UIView {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
