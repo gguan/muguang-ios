f72450f7c189a00c757fd0a5250760a06aa4827d
William Hu <william.hqs@gmail.com>
2015-04-20 14:19:13 +0800

    Add camera360 sdk reference

---
+++ MuGuangIOS/MGCamera360.mm
@@ -0,0 +1,8 @@
+//
+//  MGCamera360.c
+//  MuGuangIOS
+//
+//  Created by William Hu on 4/20/15.
+//  Copyright (c) 2015 北京目光璀璨科技有限公司. All rights reserved.
+//
+
---
+++ MuGuangIOS/MGPublisherViewController.swift
@@ -0,0 +1,37 @@
+//
+//  MGPublisherViewController.swift
+//  MuGuangIOS
+//
+//  Created by William Hu on 4/20/15.
+//  Copyright (c) 2015 北京目光璀璨科技有限公司. All rights reserved.
+//
+
+import UIKit
+
+
+
+class MGPublisherViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
